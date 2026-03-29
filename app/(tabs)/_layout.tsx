c6c8f5b699f696b575218d99c16ed713e2b18823
ScuBix1 <bastian.monnin1@gmail.com>
2026-03-29 21:36:22 +0200

    chore: rename tabs layout file

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -27,7 +27,7 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name='explore'
+        name='stats'
         options={{
           title: 'Stats',
           tabBarIcon: ({ color }) => (
